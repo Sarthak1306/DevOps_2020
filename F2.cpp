62334f81b3a36b1259e4be0ae398bfcd77d23645
Sarthak1306 <sarthak13062001@gmail.com>
2020-02-25 16:14:56 +0530

    CPP File for String Handling

---
+++ F2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(int argc, char const *argv[]) {
+  string a;
+ string b;
+
+ cin >>a;
+ cin >> b;
+ cout << a.size() <<" "<<b.size()<<endl;
+ cout << a + b<< endl;
+ cout <<b.front() + a.substr(1, a.size()-1) << " "<< a.front() + b.substr(1, b.size()-1);
+
+  return 0;
+}
