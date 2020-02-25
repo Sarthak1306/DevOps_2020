ce5d31506591359a13f2cd0f1253a28aefce2356
Sarthak1306 <sarthak13062001@gmail.com>
2020-02-25 16:26:19 +0530

    Third Set of Commit, Modified F1 : L4 Text Changed to Sarthak Srivastava

--- F1.java
+++ F1.java
@@ -1,6 +1,6 @@
 class Sarthak { //Declaration Of Class
 public static void main(String args[]){ // Declaration of Public static void public static void main
 
-    System.out.println("Hello World"); // Command to Output Message
+    System.out.println("Sarthak Srivastava!"); // Command to Output Message
 }
 }
