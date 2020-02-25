1e1ce07039ad89eb914428b3247b347199e2f1b7
Sarthak1306 <sarthak13062001@gmail.com>
2020-02-25 16:18:32 +0530

    Comments Added at L2,L3

--- F1.java
+++ F1.java
@@ -1,5 +1,5 @@
 class A {
-public static void main(String args[]){
-    System.out.println("Hello World");
+public static void main(String args[]){ // Declaration of PSVM
+    System.out.println("Hello World"); // Command to Output Message
 }
 }
